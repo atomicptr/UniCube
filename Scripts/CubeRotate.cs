e4e93b6cfa52eb65bd0f94cf289fff18d059ada5
Christopher Kaster <ikasoki@gmail.com>
2013-12-12 16:59:57 +0100

    name cubes and add rotation script

---
+++ Scripts/CubeRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour {
+
+	void Start () {
+		this.transform.eulerAngles = new Vector3(-45, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(new Vector3(0, 1, 0), 40 * Time.deltaTime);
+	}
+}
