52651a20b1b9d8d7d54d99c7461783dbc7e746d5
Christopher Kaster <ikasoki@gmail.com>
2013-12-12 15:35:36 +0100

    add basic cube

---
+++ Editor/PlanesCubeEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PlanesCube))]
+public class PlanesCubeEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		PlanesCube script = (PlanesCube)target;
+
+		if(GUILayout.Button("Setup planes")) {
+			script.SetUpPlanes();
+		}
+	}
+}
---
+++ Scripts/PlanesCube.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanesCube : MonoBehaviour {
+
+	public Material top;
+	public Material bottom;
+	public Material left;
+	public Material right;
+	public Material front;
+	public Material back;
+
+	public void SetUpPlanes() {
+		for(int i = 0; i < this.transform.childCount; i++) {
+			Transform child = this.transform.GetChild(i);
+
+			setPlane(child);
+		}
+	}
+
+	private void setPlane(Transform t) {
+		switch (t.name) {
+			case "top":
+				setTexture(t, top);
+				break;
+			case "bottom":
+				setTexture(t, bottom);
+				break;
+			case "left":
+				setTexture(t, left);
+				break;
+			case "right":
+				setTexture(t, right);
+				break;
+			case "front":
+				setTexture(t, front);
+				break;
+			case "back":
+				setTexture(t, back);
+				break;
+			default:
+				Debug.LogError("Unknown plane name: " + t.name);
+				break;
+		}
+	}
+
+	private void setTexture(Transform t, Material m) {
+		t.renderer.material = m;
+	}
+}
